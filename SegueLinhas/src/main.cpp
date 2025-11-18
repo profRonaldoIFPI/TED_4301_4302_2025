ce6d5b1668b8c435cd51127be944fc0aa3b6b101
Ronaldo Borges <ronaldo.pb@ifpi.edu.br>
2025-11-18 15:44:41 -0300

    Refactor motor control logic and adjust sensor definitions for line following functionality

--- SegueLinhas/src/main.cpp
+++ SegueLinhas/src/main.cpp
@@ -8,38 +8,42 @@
 */
 #include <Arduino.h>
 
-#define EN_D 3 // Velocidade da direita (canal A)
-#define IN1  5 // direção do motor A 
-#define IN2  4 // direção do motor A
+#define EN_D 3  // Velocidade da direita (canal A)
+#define IN1 5   // direção do motor A
+#define IN2 4   // direção do motor A
 
-#define EN_E 6 // Velocidade da esquerda (canal B)
-#define IN3  8 // direção do motor B 
-#define IN4  7 // direção do motor B
+#define EN_E 6  // Velocidade da esquerda (canal B)
+#define IN3 8   // direção do motor B
+#define IN4 7   // direção do motor B
 
-#define SENSOR_DIR A1
-#define SENSOR_ESQ A0
+#define SENSOR_DIR A0
+#define SENSOR_ESQ A1
 /* 
   Considerando uma bateria de 12v e motores de 6v,
   é bom respeitar o limite de tensão dos motores para que não queimem, 
   por isso a velocidade máxima 128 (50% de 255).
   Ajuste conforme a tensão da bateria.
 */
-#define velocidade 128 // Velocidade máxima dos motores 
-
+const int velocidade = 90;  // Velocidade máxima dos motores
+int compensacaoEsq = -5;    // Compensação para o motor mais lento (+ = esquedo, - = direito)
 /* 
   Limiar para detecção da linha.
   Ajustar conforme luz ambiente, cor da superficie ou distancia dos sensores em relação à superfície.
 */
-#define limiar 300 // branco < limiar < preto
+#define limiar 300  // branco < limiar < preto
 
 void setup() {
 
   for (int porta = 2; porta <= 8; ++porta) {
     pinMode(porta, OUTPUT);
   }
-  
+
   pinMode(SENSOR_DIR, INPUT);
   pinMode(SENSOR_ESQ, INPUT);
+
+  //sentido de rotação para frente em ambos os motores
+  analogWrite(EN_D, velocidade);  // Velocidade do motor direito
+  analogWrite(EN_E, velocidade + compensacaoEsq);
 
   // Serial.begin(9600);
 }
@@ -55,43 +59,33 @@ void loop() {
 
   // Condições para seguir a linha
   if (valorSensorDir < limiar && valorSensorEsq < limiar) {
-    // linha no centro - seguir em frente
+    digitalWrite(IN1, HIGH);  //direita
+    digitalWrite(IN2, LOW);
+
+    digitalWrite(IN3, HIGH);  //esquerda
+    digitalWrite(IN4, LOW);
+  }
+  if (valorSensorDir < limiar && valorSensorEsq >= limiar) {
     digitalWrite(IN1, HIGH);
     digitalWrite(IN2, LOW);
-    analogWrite(EN_D, velocidade); // Velocidade do motor direito
-
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+  }
+  if (valorSensorDir >= limiar && valorSensorEsq < limiar) {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
     digitalWrite(IN3, HIGH);
     digitalWrite(IN4, LOW);
-    analogWrite(EN_E, velocidade); // Velocidade do motor esquerdo
-  } 
-  else if (valorSensorDir < limiar && valorSensorEsq >= limiar) {
-    // Sensor da esquerda detecta a linha - virar à esquerda
+  }
+  if (valorSensorDir >= limiar && valorSensorEsq > limiar) {
+    analogWrite(EN_D, 0);  // Velocidade do motor direito
     digitalWrite(IN1, HIGH);
-    digitalWrite(IN2, LOW);
-    analogWrite(EN_D, velocidade); // Velocidade do motor direito
-
+    digitalWrite(IN2, HIGH);
+    analogWrite(EN_E, 0);  // Velocidade do motor esquerdo
     digitalWrite(IN3, HIGH);
-    digitalWrite(IN4, LOW);
-    analogWrite(EN_E, velocidade/2); // Reduz a velocidade do motor esquerdo
-  } 
-  else if (valorSensorDir >= limiar && valorSensorEsq < limiar) {
-    // Sensor da direita detecta a linha - virar à direita
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN2, LOW);
-    analogWrite(EN_D, velocidade/2); // Reduz a velocidade do motor direito
-
-    digitalWrite(IN3, HIGH);
-    digitalWrite(IN4, LOW);
-    analogWrite(EN_E, velocidade); // Velocidade do motor esquerdo
-  } 
-  else {
-    // Nenhum sensor detecta a linha - parar 
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN2, LOW);
-    analogWrite(EN_D, 0); // Para o motor direito
-
-    digitalWrite(IN3, HIGH);
-    digitalWrite(IN4, LOW);
-    analogWrite(EN_E, 0); // Para o motor esquerdo
+    digitalWrite(IN4, HIGH);
+  } else {
+    analogWrite(EN_D, velocidade);
+    analogWrite(EN_E, velocidade + compensacaoEsq);
   }
 }
